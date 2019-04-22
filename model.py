eb9fb1896c5d802dfc97420e6fa8ccde8010e6a2
unknown <pwb1997@hotmail.com>
2019-04-22 18:13:28 +0800

    commit

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import sklearn, h5py
 import numpy as np
+import tensorflow as tf
 
 class Model_SVM:
     
@@ -11,7 +12,7 @@ class Model_SVM:
         self.x_test = None
         self.y_test = None
         self.test_size = test_size
-        self.model = sklearn.svm.SVC(gamma="scale", C = 10)
+        self.model = sklearn.svm.SVC(kernel="linear", gamma="scale", C = 10)
 
     def split_train_test(self):
         self.x_train, self.x_test, self.y_train, self.y_test = sklearn.model_selection.train_test_split(self.features, self.labels, test_size=self.test_size, random_state=np.random)
@@ -21,3 +22,12 @@ class Model_SVM:
     
     def get_score(self):
         return self.model.score(self.x_test, self.y_test)
+
+class cnn:
+    
+    def __init__(self, labels, features, test_size):
+        self.labels = labels
+        self.features = features
+        self.test_size = test_size
+    
+
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import utils, model, sklearn, h5py
 import numpy as np
+import matplotlib.pyplot as plt
 
 scores = []
 scores_mfcc = []
@@ -7,20 +8,31 @@ scores_mfcc = []
 utils.init()
 
 # accuracy based on n_mfcc
+def n_mfcc():
+    f = h5py.File("outputs/mfcc_svm.h5", "w")
+    for n_mfcc in range(1, 50):
+        print(n_mfcc)
+        features, labels = utils.extract_features(n_mfcc, flatten=True)[:2]
+        for i in range(10):
+            print(n_mfcc, i)
+            svm = model.Model_SVM(labels, features, 0.33)
+            svm.split_train_test()
+            svm.train()
+            score = svm.get_score()
+            scores.append(score)
+        print(sum(scores)/len(scores))
+        scores_mfcc.append(sum(scores)/len(scores))
+    f.create_dataset(name="accuracy/n_mfcc", data=scores_mfcc)
+    f.close()
 
-f = h5py.File("outputs/mfcc_svm.h5", "w")
+def plot_n_mfcc():
+    with h5py.File("outputs/mfcc_svm.h5", "r") as f:
+        y = f["accuracy/n_mfcc"][()]
+        x = np.arange(1, len(y)+1)
+    plt.plot(x, y)
+    plt.xlabel("n_mfcc")
+    plt.ylabel("accuracy")
+    plt.show()
 
-for n_mfcc in range(1, 51):
-    print(n_mfcc)
-    features, labels, e = utils.extract_features(n_mfcc, flatten=True)
-    for i in range(10):
-        print(n_mfcc, i)
-        svm = model.Model_SVM(labels, features, 0.33)
-        svm.split_train_test()
-        svm.train()
-        score = svm.get_score()
-        scores.append(score)
-    print(sum(scores)/len(scores))
-    scores_mfcc.append(sum(scores)/len(scores))
-
-f.create_dataset(name="accuracy/n_mfcc", data=scores_mfcc)
+n_mfcc()
+#plot_n_mfcc()
